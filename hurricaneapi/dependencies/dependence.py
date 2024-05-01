b17004a1e57458b20228f54774a01f775a5656fe
kaMata0 <daniil49926@gmail.com>
2024-05-01 14:21:10 +0300

    added reading env file in dependence protocol

--- hurricaneapi/dependencies/dependence.py
+++ hurricaneapi/dependencies/dependence.py
@@ -3,7 +3,13 @@
 class DependenceProtocol:
     _inst = None
     def __init__(self):
-        env_file_path: str = '.env.hurricaneapi'  # noqa
+        self.env_file_path: str = '.env.hurricaneapi'
+        self.env_var: dict[str, str] = {}
+        with open(self.env_file_path, 'r') as file:
+            while line := file.readline().replace(' ', '').replace('\n', '').split('='):
+                if len(line) != 2:
+                    break
+                self.env_var[line[0]] = line[1]
 
     def __new__(cls, *args, **kwargs):
         if not isinstance(cls._inst, cls):
